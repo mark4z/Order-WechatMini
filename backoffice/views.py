14b5bebded9ef7f1195f785f09c22ad6dcd69922
Anymre <anymre@github.com>
2019-04-02 21:03:58 +0800

    订单部分完成

--- backoffice/views.py
+++ backoffice/views.py
@@ -4,6 +4,7 @@ import time
 
 from django.core import serializers
 from django.core.paginator import Paginator
+from django.db import transaction
 from django.http import HttpResponse
 from django.shortcuts import render
 
@@ -36,20 +37,25 @@ def order_list(request, page):
     return HttpResponse(json.dumps(result))
 
 
+@transaction.atomic()
 def creat_order(request):
+    data = json.loads(request.body)
     # 获取参数
     order_id = int(round(time.time() * 1000));
     user = User.objects.get(OpenId='guest')
-    desk = Desk.objects.get(DeskMum=int(request.POST['desk']))
-    comments = request.POST['comments']
-    Order.objects.create(OrderId=order_id, User=user, Desk=desk, Comments=comments)
+    desk = Desk.objects.get(DeskMum=data['desk'])
+    comments = data['comments']
+    order_state = data['pay']
+    cook_state = data['cook']
+    Order.objects.create(OrderId=order_id, User=user, Desk=desk, Comments=comments, OrderState=order_state,
+                         CookState=cook_state)
     # save cache in redis
     order = Order.objects.get(OrderId=order_id)
     total = 0
-    for i in order_id:
-        menu = Menu.objects.get(Name=i['name'])
-        OrderDetail.objects.create(menu=menu, order=order, Number=i['num'])
-        total += menu.Price * i['num']
+    for i in data['menus']:
+        menu = Menu.objects.get(Name=i['menu']['name'])
+        OrderDetail.objects.create(menu=menu, order=order, Number=i['menu']['value'])
+        total += menu.Price * i['menu']['value']
     order.Total = total
     order.save()
     return HttpResponse("Access")

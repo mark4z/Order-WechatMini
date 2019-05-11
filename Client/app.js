0fcef813846ee6695e45d776d9ad645d0e86cd55
Anymre <anymre@github.com>
2019-05-11 13:12:29 +0800

    Update app.js

--- Client/app.js
+++ Client/app.js
@@ -4,9 +4,13 @@ App({
     this.globalData.desk = parseInt(options.query.Desk)
     console.log("桌号:" + this.globalData.desk)
   },
+  onShow(options) {
+    this.globalData.desk = parseInt(options.query.Desk)
+    console.log("桌号:" + this.globalData.desk)
+  },
   globalData: {
-    url: "http://127.0.0.1:8090",
-    ip:"127.0.0.1:8090",
+    url: "http://120.77.45.136:8000",
+    ip:"120.77.45.136:8000",
     // url:"https://www.qqmxd.com",
     // ip:"www.qqmxd.com",
     userInfo: null,
